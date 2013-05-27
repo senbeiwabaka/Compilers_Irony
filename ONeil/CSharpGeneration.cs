0513d578cb5f328f15347209bd5f09837fac9a4f
Michael Yahner <mjy8@zips.uakron.edu>
2013-05-26 23:34:40 -0400

    See below
    this class is for the dynamically generated classes from the user

---
+++ ONeil/CSharpGeneration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ONeil
+{
+    public class CSharpGeneration
+    {
+        private CompilerResults _results;
+
+
+    }
+}
