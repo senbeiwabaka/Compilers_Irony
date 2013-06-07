5feb10d03ac8406292b23037fc1a771d6dfbb33d
Michael Yahner <mjy8@zips.uakron.edu>
2013-06-06 20:42:13 -0400

    methods for if and for
    now successfully converts O'Neil code into C# code for most cases and
    compiles for most cases

--- ONeil/CSharpCode.cs
+++ ONeil/CSharpCode.cs
@@ -37,115 +37,132 @@ namespace ONeil
         /// </summary>
         public void GenerateCode()
         {
-            foreach (var child in _tree.Root.ChildNodes[0].ChildNodes)
+            //foreach (var child in _tree.Root.ChildNodes[0].ChildNodes)
+            //{
+
+            _cSharpCode.Add("using System;");
+            //_cSharpCode.Add("namespace test");
+            //_cSharpCode.Add("{");
+            _cSharpCode.Add("public class Test");
+            _cSharpCode.Add("{");
+            _cSharpCode.Add("\t static void Main()");
+            _cSharpCode.Add("\t{");
+
+            foreach (var item in _tree.Root.ChildNodes[0].ChildNodes[1].ChildNodes)
             {
-                if (child.Term.Name.Equals("end"))
+                if (item.ChildNodes[0].Term.Name.Equals("int"))
                 {
-                    _cSharpCode.Add("\t}");
-                    _cSharpCode.Add("}");
+                    _cSharpCode.Add(item.FindTokenAndGetText() + " " + item.ChildNodes[1].FindTokenAndGetText() + ";");
                     continue;
                 }
-                foreach (var children in child.ChildNodes)
+                else if (item.ChildNodes[0].Term.Name.Equals("list"))
                 {
-                    if (children.Term.Name.Equals("var"))
-                    {
-                        _cSharpCode.Add("using System;");
-                        //_cSharpCode.Add("namespace test");
-                        //_cSharpCode.Add("{");
-                        _cSharpCode.Add("public class Test");
-                        _cSharpCode.Add("{");
-                        _cSharpCode.Add("\t static void Main()");
-                        _cSharpCode.Add("\t{");
-                        continue;
-                    }
-                    
-                    if (children.ChildNodes.Count <= 0)
-                    {
-                        continue;
-                    }
-                    if (children.Term.Name.Equals("varDecl"))
-                    {
-                        if (children.ChildNodes[0].Term.Name.Equals("int"))
-                        {
-                            _cSharpCode.Add(children.FindTokenAndGetText() + " " + children.ChildNodes[1].FindTokenAndGetText() + ";");
-                            continue;
-                        }
-                        else if (children.ChildNodes[0].Term.Name.Equals("list"))
-                        {
-                            _cSharpCode.Add("int[] " + children.ChildNodes[2].FindTokenAndGetText() +
-                                " = new int[" + children.ChildNodes[1].FindTokenAndGetText() + "];");
-                            continue;
-                        }
-                    }
-
-                    NonControlFlow(children);
-
-                    if (children.Term.Name.Equals("while"))
-                    {
-                        WhileMethod(children);
-                        continue;
-                    }
-                    else if (children.Term.Name.Equals("if"))
-                    {
-                        FlowConditionalStatements(children);
-                        _cSharpCode[_cSharpCode.Count - 1] += ")";
-                        if (children.ChildNodes.Count == 6)
-                        {
-                            NonControlFlow(children.ChildNodes[5]);
-                        }
-                    }
-                    else if (children.Term.Name.Equals("for"))
-                    {
-                        _cSharpCode.Add("for (" + children.ChildNodes[1].FindTokenAndGetText() + " = ");
-                        var index = _cSharpCode.Count - 1;
-                        var first = false;
-                        ChildrenNodesRetrevial(children.ChildNodes[3], ref index, ref first);
-                        _cSharpCode[index] += "; " + children.ChildNodes[1].FindTokenAndGetText() + " <= ";
-                        ChildrenNodesRetrevial(children.ChildNodes[5], ref index, ref first);
-                        _cSharpCode[index] += "; " + children.ChildNodes[1].FindTokenAndGetText() + "++){";
-                        for (int i = 0; i < children.ChildNodes[6].ChildNodes.Count; i++)
-                        {
-                            if (children.ChildNodes[6].ChildNodes[i].Term.Name.Equals("if") ||
-                                children.ChildNodes[6].ChildNodes[i].Term.Name.Equals("while"))
-                            {
-                                FlowConditionalStatements(children.ChildNodes[6].ChildNodes[i]);
-                            }
-                            else
-                            {
-                                NonControlFlow(children.ChildNodes[6].ChildNodes[i]);
-                            }
-                        }
-                        _cSharpCode.Add("}");
-                    }
+                    _cSharpCode.Add("int[] " + item.ChildNodes[4].FindTokenAndGetText() +
+                        " = new int[" + item.ChildNodes[2].FindTokenAndGetText() + "];");
+                    continue;
                 }
+            }
+
+            foreach (var children in _tree.Root.ChildNodes[0].ChildNodes[3].ChildNodes)
+            {
+                if (children.Term.Name.Equals("varDecl"))
+                {
+
+                }
+
+                NonControlFlow(children);
+
+                if (children.Term.Name.Equals("while"))
+                {
+                    WhileMethod(children);
+                    continue;
+                }
+                else if (children.Term.Name.Equals("if"))
+                {
+                    IfMethod(children);
+                }
+                else if (children.Term.Name.Equals("for"))
+                {
+                    ForMethod(children);
+                }
+            }
+            //}
+
+            _cSharpCode.Add("\t}");
+            _cSharpCode.Add("}");
+        }
+
+        private void ForMethod(ParseTreeNode children)
+        {
+            _cSharpCode.Add("for (" + children.ChildNodes[1].FindTokenAndGetText() + " = ");
+            var index = _cSharpCode.Count - 1;
+            ChildrenNodesRetrevial(children.ChildNodes[3], ref index);
+            _cSharpCode[index] += "; " + children.ChildNodes[1].FindTokenAndGetText() + " <= ";
+            ChildrenNodesRetrevial(children.ChildNodes[5], ref index);
+            _cSharpCode[index] += "; " + children.ChildNodes[1].FindTokenAndGetText() + "++)";
+            _cSharpCode.Add("{");
+            for (int i = 0; i < children.ChildNodes[6].ChildNodes.Count; i++)
+            {
+                if (children.ChildNodes[6].ChildNodes[i].Term.Name.Equals("if"))
+                {
+                    FlowConditionalStatements(children.ChildNodes[6].ChildNodes[i]);
+                }
+                else if (children.ChildNodes[6].ChildNodes[i].Term.Name.Equals("while"))
+                {
+                    WhileMethod(children.ChildNodes[6].ChildNodes[i]);
+                }
+                else
+                {
+                    NonControlFlow(children.ChildNodes[6].ChildNodes[i]);
+                }
+            }
+            _cSharpCode.Add("}");
+        }
+
+        private void IfMethod(ParseTreeNode node)
+        {
+            var index = 0;
+            _cSharpCode.Add("if ( ");
+            index = _cSharpCode.Count - 1;
+            ChildrenNodesRetrevial(node.ChildNodes[2], ref index);
+            ChildrenNodesRetrevial(node.ChildNodes[3], ref index);
+            ChildrenNodesRetrevial(node.ChildNodes[4], ref index);
+            _cSharpCode[index] += ")";
+            //FlowConditionalStatements(node);
+            //_cSharpCode[_cSharpCode.Count - 1] += ")";
+            if (node.ChildNodes.Count == 8)
+            {
+                NonControlFlow(node.ChildNodes[7]);
             }
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="children"></param>
-        private void WhileMethod(ParseTreeNode children)
+        /// <param name="node"></param>
+        private void WhileMethod(ParseTreeNode node)
         {
-            FlowConditionalStatements(children);
-            _cSharpCode[_cSharpCode.Count - 1] += "){";
-
-            foreach (var item in children.ChildNodes[4].ChildNodes)
+            var index = 0;
+            _cSharpCode.Add("while (");
+            index = _cSharpCode.Count - 1;
+            ChildrenNodesRetrevial(node.ChildNodes[2], ref index);
+            _cSharpCode[index] += node.ChildNodes[3].FindTokenAndGetText();
+            ChildrenNodesRetrevial(node.ChildNodes[4], ref index);
+            _cSharpCode[_cSharpCode.Count - 1] += ")";
+            _cSharpCode.Add("{");
+            foreach (var item in node.ChildNodes[6].ChildNodes)
             {
                 if (item.Term.Name.Equals("if"))
                 {
-                    FlowConditionalStatements(item);
-                    _cSharpCode[_cSharpCode.Count - 1] += ")";
-                    if (item.ChildNodes.Count == 6)
-                    {
-                        NonControlFlow(item.ChildNodes[5]);
-                    }
+                    IfMethod(item);
                 }
                 else if (item.Term.Name.Equals("while"))
                 {
-                    FlowConditionalStatements(children);
-                    _cSharpCode[_cSharpCode.Count - 1] += "){";
                     WhileMethod(item);
+                }
+                else if (item.Term.Name.Equals("for"))
+                {
+                    ForMethod(item);
                 }
                 else
                 {
@@ -158,64 +175,53 @@ namespace ONeil
         /// <summary>
         /// Does labels, prints, prompts, gotos
         /// </summary>
-        /// <param name="children">Takes a node and checks for the above stated</param>
-        private void NonControlFlow(ParseTreeNode children)
+        /// <param name="node">Takes a node and checks for the above stated</param>
+        private void NonControlFlow(ParseTreeNode node)
         {
-            if (children.ChildNodes[0].Term.Name.Equals("label"))
+            if (node.ChildNodes[0].Term.Name.Equals("label"))
             {
-                _cSharpCode.Add(children.ChildNodes[1].FindTokenAndGetText() + ":");
+                _cSharpCode.Add(node.ChildNodes[1].FindTokenAndGetText() + ":");
             }
-            else if (children.ChildNodes[0].Term.Name.Equals("input"))
+            else if (node.ChildNodes[0].Term.Name.Equals("input"))
             {
-                _cSharpCode.Add(children.ChildNodes[1].FindTokenAndGetText() + " = Convert.ToInt32(Console.ReadLine());");
+                _cSharpCode.Add(node.ChildNodes[1].FindTokenAndGetText() + " = Convert.ToInt32(Console.ReadLine());");
             }
-            else if (children.ChildNodes[0].Term.Name.Equals("goto"))
+            else if (node.ChildNodes[0].Term.Name.Equals("goto"))
             {
-                _cSharpCode.Add(children.ChildNodes[0].FindTokenAndGetText() + " " + children.ChildNodes[1].FindTokenAndGetText() + ";");
+                _cSharpCode.Add(node.ChildNodes[0].FindTokenAndGetText() + " " + node.ChildNodes[1].FindTokenAndGetText() + ";");
             }
-            else if (children.ChildNodes[0].Term.Name.Equals("prompt"))
+            else if (node.ChildNodes[0].Term.Name.Equals("prompt"))
             {
-                _cSharpCode.Add("Console.Write(" + children.ChildNodes[1].FindTokenAndGetText() + ");");
+                _cSharpCode.Add("Console.Write(" + node.ChildNodes[1].FindTokenAndGetText() + ");");
             }
-            else if (children.ChildNodes[0].Term.Name.Equals("let"))
+            else if (node.ChildNodes[0].Term.Name.Equals("let"))
             {
-                _cSharpCode.Add(children.ChildNodes[1].FindTokenAndGetText());
+                _cSharpCode.Add(node.ChildNodes[1].FindTokenAndGetText());
                 var index = _cSharpCode.Count - 1;
-                var firstAdd = false;
-                if (children.ChildNodes.Count <= 4)
+                if (node.ChildNodes.Count <= 4)
                 {
                     _cSharpCode[index] += " = ";
-                    for (int i = 3; i < children.ChildNodes.Count; i++)
-                    {
-                        ChildrenNodesRetrevial(children.ChildNodes[i], ref index, ref firstAdd);
-                    }
-
+                    ChildrenNodesRetrevial(node.ChildNodes[3], ref index);
                 }
                 else
                 {
                     _cSharpCode[index] += " [";
-                    for (int i = 2; i < children.ChildNodes.Count; i++)
-                    {
-                        if (i == 3)
-                        {
-                            _cSharpCode[index] += "] ";
-                        }
-                        ChildrenNodesRetrevial(children.ChildNodes[i], ref index, ref firstAdd);
-                    }
+                    ChildrenNodesRetrevial(node.ChildNodes[3], ref index);
+                    _cSharpCode[index] += "] " + node.ChildNodes[5].FindTokenAndGetText();
+                    ChildrenNodesRetrevial(node.ChildNodes[6], ref index);
                 }
 
                 _cSharpCode[index] += ";";
             }
-            else if (children.ChildNodes[0].Term.Name.Equals("print"))
+            else if (node.ChildNodes[0].Term.Name.Equals("print"))
             {
                 _cSharpCode.Add("Console.Write(");
                 var index = _cSharpCode.Count - 1;
-                var first = false;
-                ChildrenNodesRetrevial(children.ChildNodes[1], ref index, ref first);
-                if (children.ChildNodes.Count > 2)
+                ChildrenNodesRetrevial(node.ChildNodes[1], ref index);
+                if (node.ChildNodes.Count > 2)
                 {
                     _cSharpCode[index] += "[";
-                    ChildrenNodesRetrevial(children.ChildNodes[2], ref index, ref first);
+                    ChildrenNodesRetrevial(node.ChildNodes[3], ref index);
                     _cSharpCode[index] += "]";
                 }
                 
@@ -226,24 +232,24 @@ namespace ONeil
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="children"></param>
-        private void FlowConditionalStatements(ParseTreeNode children)
+        /// <param name="node"></param>
+        private void FlowConditionalStatements(ParseTreeNode node)
         {
             bool first = true;
             int index = 0;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < node.ChildNodes.Count; i++)
             {
                 if (first)
                 {
-                    _cSharpCode.Add(children.FindTokenAndGetText() + "(");
+                    _cSharpCode.Add(node.FindTokenAndGetText() + "(");
                     index = _cSharpCode.Count - 1;
                     first = false;
                 }
                 else
                 {
-                    foreach (var item in children.ChildNodes[i].ChildNodes)
+                    foreach (var item in node.ChildNodes[i].ChildNodes)
                     {
-                        ChildrenNodesRetrevial(item, ref index, ref first);
+                        ChildrenNodesRetrevial(item, ref index);
                     }
                 }
             }
@@ -254,31 +260,19 @@ namespace ONeil
         /// </summary>
         /// <param name="node"></param>
         /// <param name="index"></param>
-        /// <param name="firstAdd"></param>
-        private void ChildrenNodesRetrevial(ParseTreeNode node, ref int index, ref bool firstAdd)
+        /// 
+        private void ChildrenNodesRetrevial(ParseTreeNode node, ref int index)
         {
             if (node.ChildNodes.Count > 0)
             {
                 foreach (var item in node.ChildNodes)
                 {
-                    ChildrenNodesRetrevial(item, ref index, ref firstAdd);
+                    ChildrenNodesRetrevial(item, ref index);
                 }
             }
-            else
+            else if (node.FindTokenAndGetText() != null)
             {
-                if (firstAdd)
-                {
-                    _cSharpCode.Add(node.FindTokenAndGetText() + " ");
-                    index = _cSharpCode.Count - 1;
-                    firstAdd = false;
-                }
-                else
-                {
-                    if (node.FindTokenAndGetText() != null)
-                    {
-                        _cSharpCode[index] = _cSharpCode[index] + node.FindTokenAndGetText() + " ";
-                    }
-                }
+                _cSharpCode[index] = _cSharpCode[index] + node.FindTokenAndGetText() + " ";
             }
         }
 
